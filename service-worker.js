a887af52d4b1a4908d2ac79ddc481ceb36f882c2
AutoLeadsapp <automacaoleads.app@gmail.com>
2026-08-11 19:49:02 -0300

    a cemra ianda nao da certo, concertando

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gundula-certa-v7';
+const CACHE = 'gundula-certa-v8';
 const ARQUIVOS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icone.svg'];
 self.addEventListener('install', evento => evento.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ARQUIVOS))));
 self.addEventListener('activate', evento => evento.waitUntil(caches.keys().then(chaves => Promise.all(chaves.filter(chave => chave !== CACHE).map(chave => caches.delete(chave)))).then(() => self.clients.claim())));
