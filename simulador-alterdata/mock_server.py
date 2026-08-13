a8d18cc632437cbb1003e89c3192c167b43c9251
AutoLeadsapp <automacaoleads.app@gmail.com>
2026-08-12 21:27:59 -0300

    ajustes 1

--- simulador-alterdata/mock_server.py
+++ simulador-alterdata/mock_server.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app) # Isso libera o acesso para que o seu app consiga fazer requisições
+
+# Carrega os dados do JSON
+DATA_FILE = 'mock_data.json'
 
 # Carrega os dados do JSON
 DATA_FILE = 'mock_data.json'
